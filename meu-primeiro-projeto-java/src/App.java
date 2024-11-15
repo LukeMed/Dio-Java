98431a52b7143210285760d1027459bcd96f9a90
LukeMed <luke.lam1988@gmail.com>
2024-11-15 17:43:40 -0300

    Atualização 15//11/2024

--- meu-primeiro-projeto-java/src/App.java
+++ meu-primeiro-projeto-java/src/App.java
@@ -1,5 +1,8 @@
+
 public class App {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Welcome, Dio class! Let's go to study!");
+        String aluno = "Lucas";
+        System.out.println("Welcome, Dio class! Let's go to study with " + aluno + ". Atualização do dia 15/11/2024");
     }
 }
